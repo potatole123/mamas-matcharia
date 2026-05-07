5fb32cf2f80899c52d75eb8b8904424733c967f6
David Budziwojski <dsbudziwojski@gmail.com>
2026-05-07 15:47:39 -0700

    SERVER: added server side firebase authentication

---
+++ server/src/authenticate.ts
@@ -0,0 +1,22 @@
+import admin from "./firebaseAdmin";
+import type { Request, Response, NextFunction } from "express";
+
+
+export default async function authenticate(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({message: 'Unauthorized: No token provided'});
+    }
+
+    const idToken = authHeader.split('Bearer ')[1] as string;
+
+    try{
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        req.user=decodedToken;
+        next();
+    } catch (error){
+        console.error('Error verifying token:', error);
+        return res.status(401).json({message: 'Unauthorized: Invalid token'});
+    }
+}
