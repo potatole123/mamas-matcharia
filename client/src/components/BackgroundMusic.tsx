a935052b0df94a5c94a548a65f5ba3ae06713a6e
potatole123 <nathanle2003@gmail.com>
2026-06-05 13:50:56 -0700

    Added Unmuted Asset.

--- client/src/components/BackgroundMusic.tsx
+++ client/src/components/BackgroundMusic.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import backgroundTrack from '../music/magnific-cachito.mp3'
-import muteButtonImage from '../music/b7e9f4f2-e274-474d-bb23-1cf82641a1da.png'
+import mutedButtonImage from '../music/b7e9f4f2-e274-474d-bb23-1cf82641a1da.png'
+import unmutedButtonImage from '../music/3c26ffa9-f70b-45a9-a6f1-1fbe315a9884.png'
 import './BackgroundMusic.css'
 
 const MUSIC_MUTED_STORAGE_KEY = 'mamas-matcharia-music-muted'
@@ -82,7 +83,11 @@ function BackgroundMusic() {
         aria-pressed={isMuted}
         onClick={handleToggleMute}
       >
-        <img src={muteButtonImage} alt="" draggable="false" />
+        <img
+          src={isMuted ? mutedButtonImage : unmutedButtonImage}
+          alt=""
+          draggable="false"
+        />
       </button>
     </>
   )
